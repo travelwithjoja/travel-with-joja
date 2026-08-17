2884924d5b38fdcdda9c7d3e5e284ec104f2487e
travelwithjoja <stheekshana37@gmail.com>
2026-08-17 14:33:26 +0530

    Fix async submit

--- src/components/BespokeTripPlanner.tsx
+++ src/components/BespokeTripPlanner.tsx
@@ -87,7 +87,7 @@ export const BespokeTripPlanner: React.FC<BespokeTripPlannerProps> = ({
     notes
   });
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
   e.preventDefault();
 
   const plan = getFullPlan();
